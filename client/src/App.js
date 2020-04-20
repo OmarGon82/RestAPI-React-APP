aa99983c420f928a2c63bb14383fb73fcebb86ca
Omar Gonzalez <gonzalezoe82@gmail.com>
2020-04-20 17:40:50 +0900

    Header now displaying

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+// Component Imports
+import Header from './Components/Header';
 
+export default () => (
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoaded: false,
-    }
-  }
+    <Router>
+      <div>
+        <Header />
+      </div>
+    </Router>
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     items: [],
+  //     isLoaded: false,
+  //   }
+  // }
   
-  componentDidMount() {
-    fetch('http://localhost:5000/api/courses')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          isLoaded: true,
-          items: json,
-        })
-      });
-  }
-  render() {
-    const { isLoaded, items} = this.state;
-    if(!isLoaded) {
-      return <div>Loading...</div>;
-    }
-    else {
-      return (
-        <div className="App">
-            {items.map(item => (
-              <div key={item.id}>
-                Course: {item.title}
-                id:  {item.id}
-              </div>
-            ))}
+  // componentDidMount() {
+  //   fetch('http://localhost:5000/api/courses')
+  //     .then(res => res.json())
+  //     .then(json => {
+  //       this.setState({
+  //         isLoaded: true,
+  //         items: json,
+  //       })
+  //     });
+  // }
+  // render() {
+  //   const { isLoaded, items} = this.state;
+  //   if(!isLoaded) {
+  //     return <div>Loading...</div>;
+  //   }
+  //   else {
+  //     return (
+  //       <div >
+  //         <h1>Results</h1>
+  //           {items.map(item => (
+  //             <div>
+  //             <h1 className="course--header" key={item.id}>
+  //               Course: {item.title}
+  //             </h1>
+  //             <h2>
+  //               id:  {item.id}
+  //             </h2>
+  //             </div>
+  //           ))}
   
-        </div>  
-      )
-    }
-  }
+  //       </div>  
+  //     )
+  //   }
+  // }
 
-}
+)
 
-export default App;
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,3 +1,24 @@
-// Header- Displays the top menu bar for the application and includes buttons 
-// for signing in and signing up (if there's not an authenticated user) or the user's first and last name 
-// and a button for signing out (if there's an authenticated user).
+import React from 'react'; 
+import { Link } from 'react-router-dom';
+
+export default class Header extends React.PureComponent {
+    render() {
+        return (
+            <div className="header">
+                <div className="bounds">
+                    <h1 class="header---logo">Full Stack App</h1>
+                    <nav>
+                    <React.Fragment>
+                        <span>Welcome, UserName!</span>
+                        <Link to="/signout">Sign Out</Link>
+                    </React.Fragment>
+                    <React.Fragment>
+                        <Link className="signup" to="/signup">Sign up</Link>
+                        <Link className="signin" to="/signin">Sign In</Link>
+                    </React.Fragment>
+                    </nav>
+                </div>
+            </div>
+        );
+    }
+}
