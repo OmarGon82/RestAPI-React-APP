9f09362857e81fb48ffd955dd2d8a864f4563fa1
Omar Gonzalez <gonzalezoe82@gmail.com>
2021-04-08 17:50:39 +0900

    update config

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,4 @@
 export default {
   // apiBaseUrl: 'http://localhost:5000/api',
-  apiBaseUrl: "https://school-database-app.herokuapp.com/",
+  apiBaseUrl: "https://school-database-app.herokuapp.com/api",
 };
